dcee10f6945729ba25f701641d4a53fe12eed813
Matie <mathias.knudsen31@gmail.com>
2016-09-16 14:10:36 +0100

    Finalisation de l'inscription #2

--- validation.php
+++ validation.php
@@ -3,7 +3,6 @@ require('config/config.php');
 require('model/functions.fn.php');
 session_start();
 
-$userRegistration = userRegistration($db,$_POST['username'],$_POST['email'], $_POST['password']);
 
 if(	isset($_POST['username']) && isset($_POST['email']) && isset($_POST['password']) && 
 	!empty($_POST['username']) && !empty($_POST['email']) && !empty($_POST['password'])) {
@@ -12,3 +11,14 @@ if(	isset($_POST['username']) && isset($_POST['email']) && isset($_POST['passwor
 	$_SESSION['message'] = 'Erreur : Formulaire incomplet';
 	header('Location: register.php');
 }
+
+$isEmailAvailable = isEmailAvailable($db, $_POST['email']);
+$isUsernameAvailable = isUsernameAvailable($db, $_POST['username']);
+
+if (isEmailAvailable($db, $_POST['email']) & isUsernameAvailable($db, $_POST['username'])){
+	$userRegistration = userRegistration($db,$_POST['username'],$_POST['email'], $_POST['password']);
+}else{
+	$_SESSION['message'] = 'Erreur : Email indisponible';
+	header('Location: register.php');	
+}
+
